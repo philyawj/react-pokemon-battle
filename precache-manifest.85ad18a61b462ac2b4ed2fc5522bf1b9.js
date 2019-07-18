057faf75e42673422f505f9eee701c3da036c5ba
Jordan Philyaw <jordanphilyaw@gmail.com>
2019-07-17 18:46:09 -0700

    Updates

---
+++ precache-manifest.85ad18a61b462ac2b4ed2fc5522bf1b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa49bbafe1f915bf717856fbb2016cc3",
+    "url": "/react-pokemon-battle/index.html"
+  },
+  {
+    "revision": "5915297393772194ed48",
+    "url": "/react-pokemon-battle/static/css/main.b82fb34a.chunk.css"
+  },
+  {
+    "revision": "931be784e137a4be6a48",
+    "url": "/react-pokemon-battle/static/js/2.2a94edb8.chunk.js"
+  },
+  {
+    "revision": "5915297393772194ed48",
+    "url": "/react-pokemon-battle/static/js/main.12153588.chunk.js"
+  },
+  {
+    "revision": "96680e1d5cfa0b4c9189",
+    "url": "/react-pokemon-battle/static/js/runtime~main.3fe7ad69.js"
+  }
+]);
